4b5dbc3b251cd36664e6847092cab63818a383e0
Alex Withington-Smith <alex@awithsmith.com>
2020-10-10 19:55:00 +0100

    passing test for two sorted

--- lib/mergesort.rb
+++ lib/mergesort.rb
@@ -1,5 +1,13 @@
 class MergeSort
   def self.sort(array)
-    array
+    if array.length < 2
+      array
+    elsif array.length == 2
+      if array[0] > array[1]
+        [array[1], array[0]]
+      else
+        array
+      end
+    end
   end
 end
