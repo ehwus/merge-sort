fcc122c36f8c425f87c96de071c93860460a4ac1
Alex Withington-Smith <alex@awithsmith.com>
2020-10-10 19:51:02 +0100

    added second test for single entry array

--- spec/mergesort_spec.rb
+++ spec/mergesort_spec.rb
@@ -5,5 +5,9 @@ describe MergeSort do
     it "returns empty array if given empty array" do
       expect(MergeSort.sort([])).to eq([])
     end
+
+    it "returns value if given array with single value" do
+      expect(MergeSort.sort([1])).to eq([1])
+    end
   end
 end
