05bf941f934d6233c629cdddf39bd4c196b924f1
Alex Withington-Smith <alex@awithsmith.com>
2020-10-11 04:32:11 +0100

    added test for 10 items in array, currently failing

--- lib/mergesort.rb
+++ lib/mergesort.rb
@@ -3,14 +3,17 @@ class MergeSort
     if array.length < 2
       array
     else
-      merge(sort(array.first(array.length / 2)), sort(array.last(array.length / 2)))
+      left, right = array.each_slice( (array.size/2.0).round ).to_a
+      merge(sort(left), sort(right))
     end
   end
 
   def self.merge(a, b)
     return_array = []
     a.each_with_index do |value, index|
-      if value > b[index]
+      if b[index].nil?
+        return_array.push(value)
+      elsif value > b[index]
         return_array.push(b[index])
         return_array.push(value)
       else
--- spec/mergesort_spec.rb
+++ spec/mergesort_spec.rb
@@ -17,6 +17,11 @@ describe MergeSort do
     it "sorts array of five values correctly" do
       expect(MergeSort.sort([3, 7, 1, 9])).to eq([1, 3, 7, 9])
     end
+
+    it "sorts array of 10 values correctly" do
+      test_list = [5, 69, 4346, 6, 69, 420, 1, 65, 5, 10]
+      expect(MergeSort.sort(test_list)).to eq([1, 5, 5, 6, 10, 65, 69, 69, 420, 4346])
+    end
   end
 
   describe "#merge" do
